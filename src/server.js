43a52a32864a3be00836208ed65780bd64f03012
subhodeep2005s <sarkarsubhodeep417@gmail.com>
2025-10-04 13:47:45 +0530

    added cron

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ import "dotenv/config";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { productRouter } from "./routes/product.route.js";
+import cron from "node-cron";
+if (!process.env.URL) {
+	throw new Error("URL is not defined in environment variables");
+}
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -19,6 +23,22 @@ app.use(express.json());
 
 app.use("/auth", authRouter);
 
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+cron.schedule("13 0 * * *", async () => {
+	console.log("Running a task every day at midnight");
+	try {
+		const res = await fetch(process.env.URL, {
+			method: "GET",
+		});
+		const data = await res.text();
+		console.log("Response:", data);
+	} catch (error) {
+		console.error("Error during fetch:", error);
+	}
+});
 // routes for products
 
 app.use("/product", productRouter);
